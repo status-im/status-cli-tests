77d0ed26142d2d6adc85e02f3b1d3509e336e880
Florin Barbu <barbu_florin_adrian@yahoo.com>
2024-06-04 11:58:07 +0300

    test packet loss

--- src/steps/common.py
+++ src/steps/common.py
@@ -29,7 +29,7 @@ class StepsCommon:
         # subprocess.Popen("sudo tc qdisc add dev eth0 root netem loss 90%", shell=True)
         # sudo tc qdisc add dev eth0 root netem delay 1s 100ms distribution normal
         # 1 sec disconnects
-        subprocess.Popen("sudo tc qdisc add dev eth0 root tbf rate 50kbit burst 16kbit latency 200ms", shell=True)
+        subprocess.Popen("sudo tc qdisc add dev eth0 root tbf rate 1mbit burst 32kbit latency 100ms", shell=True)
         yield
         subprocess.Popen("sudo tc qdisc del dev eth0 root netem", shell=True)
         self.node_alice.stop()
